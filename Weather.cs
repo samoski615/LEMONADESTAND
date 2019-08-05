d457e1d77e2366c3ee97f5da1eb56961e09758a5
samoski615 <samoski615@gmail.com>
2019-08-05 16:31:56 -0500

    initial commit

---
+++ Weather.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LemonadeStand
+{
+    public class Weather
+    {
+        //member variables
+        string degreesF;
+        string Forecast;
+
+        List<string> WeatherCondtions = new List<string>()
+        {
+            "Cloudy",
+            "Rainy",
+            "Sunny",
+            "Hazy",
+            "Hot and Humid",
+        };
+        
+        //constructor
+        public Weather()
+        {
+            
+        }
+        //member methods
+
+                   
+        
+    }
+}
