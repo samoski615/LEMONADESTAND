039a9cfb0c92d25566c844b87a9a171f341b0c25
samoski615 <samoski615@gmail.com>
2019-08-06 13:01:43 -0500

    what the hell is wrong with my computer?! It deleted my program file and my userinterface file unbeknownst to me >:(  ---> I fixed it, I think!

--- Weather.cs
+++ Weather.cs
@@ -7,36 +7,34 @@ namespace LemonadeStand
     public class Weather
     {
         //member variables
-        int degreesF;
+        int temp;
         string forecast;
-        public string[] WeatherConditions = { "Cloudy", "Sunny", "Rainy", "Hazy", "Hot and Humid" };
+        public List<string> WeatherConditions = new List<string> { "Cloudy", "Sunny", "Rainy", "Hazy", "Hot and Humid" };
 
         //constructor
         public Weather()
         {
-            int degreesF = 0;
+            int temp = 0;
             forecast = null;
             string[] WeatherConditions = { "Cloudy", "Sunny", "Rainy", "Hazy", "Hot and Humid" };
             ActualWeather();
+
         }
         //member methods
         public void ActualWeather()
         {
             Random rng = new Random();
             string[] WeatherConditions = { "Cloudy", "Sunny", "Rainy", "Hazy", "Hot and Humid" };
-
-            //Generate random indexes for weather conditions
             int mIndex = rng.Next(WeatherConditions.Length);
-            Console.WriteLine("The weather conditions for today are {0} ", WeatherConditions[mIndex]);
         }
-                   
-        public void ActualTemp()
+
+        public int ActualTemp()
         {
-            
-
-       
-
+            Random temp = new Random();
+            Console.WriteLine(temp.Next(60, 80));
+            return ActualTemp();
         }
-
     }
+
 }
+
